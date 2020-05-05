7739ee51f62a4ee09d95585c2d76050f20b40782
Damian Demasi <damian.demasi.1@gmail.com>
2020-05-05 19:01:31 +0930

    Finishing with Mongoose

--- cats.js
+++ cats.js
@@ -11,25 +11,41 @@ var catSchema = new mongoose.Schema({
 var Cat = mongoose.model('Cat', catSchema); // We can use the variable Cat to find cats, update them, remove them, etc.
 //                         ^ This 'Cat' is the singular version of the collection name (the collection will be 'Cats').
 
-// New cat
-var george = new Cat({
-    name: 'George',
-    age: 11,
-    temperament: 'Mad'
-});
-// Save the new cat to the DB (we use a callback function because the interaction betwee JS and MongoDB takes time)
-george.save(function(err, cat){
-    if(err) {
-        console.log('Something went wrong!');
-    } else {
-        console.log('Cat saved to the DB: ' + cat);
+// // New cat
+// var george = new Cat({
+//     name: 'George',
+//     age: 11,
+//     temperament: 'Mad'
+// });
+// // Save the new cat to the DB (we use a callback function because the interaction betwee JS and MongoDB takes time)
+// george.save(function(err, cat){
+//     if(err) {
+//         console.log('Something went wrong!');
+//     } else {
+//         console.log('Cat saved to the DB: ' + cat);
         
+//     }
+// });
+
+// Create an element in the DB
+Cat.create({
+    name: "Snow White",
+    age: 15,
+    temperament: "Bland"
+}, function(err, cat){
+    if(err) {
+        console.log('Something went wrong! ' + err);
+    } else {
+        console.log('Cat created in the DB: ' + cat);
     }
 });
 
+// Retrieve all cats from the DB (using the 'find' method on our 'Cat' model (line 11))
+Cat.find({}, function(err, cats){
+    if(err) {
+        console.log('Something went wrong! ' + err);
+    } else {
+        console.log('Cat(s) found in the DB: ' + cats);
+    }
+});
 
-// Adding a new cat to the DB
-
-
-
-// Retrieve all cats from the DB
