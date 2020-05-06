a903948e10d025d062c3057f6214e07005ad1e6d
Damian Demasi <damian.demasi.1@gmail.com>
2020-05-06 19:12:15 +0930

    YelpCamp into DB with mongoose

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
 // Initializing express and importing packages
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const   express     = require('express'),
+        app         = express(),
+        bodyParser  = require('body-parser'),
+        mongoose    = require('mongoose');
+
+// Connecting to mongodb DB through mongoose
+mongoose.connect('mongodb://localhost/yelp_camp_db');
+
 app.use(bodyParser.urlencoded({extended: true}));
 // Use "ejs" as view engine (so you don't have to write ".ejs" for file extensions):
 app.set('view engine', 'ejs');
 
+// Setting up the schema for mongoose and mongodb
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Adding a campground to the db
+Campground.create(
+    {
+        name: 'Unley Creek',
+        image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+    }, function(err, campground){
+        if(err) {
+            console.log('Error! ' + err);
+        } else {
+            console.log('New campground added to DB! ' + campground);
+        }
+});
+
 // Array that will later be moved into a database
-const campgrounds = [
-    {name: 'Unley Creek', image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'},
-    {name: 'Norwood Forest', image: 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1088&q=80'},
-    {name: 'Glenelg Valley', image: 'https://images.unsplash.com/photo-1548062005-e50d06091399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1252&q=80'},
-    {name: 'Unley Creek', image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'},
-    {name: 'Norwood Forest', image: 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1088&q=80'},
-    {name: 'Glenelg Valley', image: 'https://images.unsplash.com/photo-1548062005-e50d06091399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1252&q=80'},
-    {name: 'Unley Creek', image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'},
-    {name: 'Norwood Forest', image: 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1088&q=80'},
-    {name: 'Glenelg Valley', image: 'https://images.unsplash.com/photo-1548062005-e50d06091399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1252&q=80'}
-];
+// const campgrounds = [
+//     {name: 'Unley Creek', image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'},
+//     {name: 'Norwood Forest', image: 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1088&q=80'},
+//     {name: 'Glenelg Valley', image: 'https://images.unsplash.com/photo-1548062005-e50d06091399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1252&q=80'},
+//     {name: 'Unley Creek', image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'},
+//     {name: 'Norwood Forest', image: 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1088&q=80'},
+//     {name: 'Glenelg Valley', image: 'https://images.unsplash.com/photo-1548062005-e50d06091399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1252&q=80'},
+//     {name: 'Unley Creek', image: 'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'},
+//     {name: 'Norwood Forest', image: 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1088&q=80'},
+//     {name: 'Glenelg Valley', image: 'https://images.unsplash.com/photo-1548062005-e50d06091399?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1252&q=80'}
+// ];
 
 // Routes
 app.get('/', (req, res) => {
@@ -25,15 +50,35 @@ app.get('/', (req, res) => {
     res.render('landing');
 });
 app.get('/campgrounds', (req, res) => { // Using RESTful convention
-    res.render('campgrounds', {campgroundsPage:campgrounds});
+    // Get all campgrounds from DB
+    Campground.find(
+        {}, function(err, campgrounds){
+            if(err) {
+                console.log('Error! ' + err);
+            } else {
+                console.log('Found campgrounds! ' + campgrounds);
+                res.render('campgrounds', {campgroundsPage:campgrounds});
+            }
+    });
+
+    
+    // Using arrays: res.render('campgrounds', {campgroundsPage:campgrounds});
 });
 app.post('/campgrounds', (req, res) => { // Using RESTful convention
     // res.send('Posted!');
     const campgroundName = req.body.campgroundName;
     const campgroundImage = req.body.campgroundImage;
     const newCampGround = {name: campgroundName, image: campgroundImage};
-    campgrounds.push(newCampGround);
-    res.redirect('/campgrounds'); // Default redirection is as GET request
+    // If using arrays: campgrounds.push(newCampGround);
+    // Add a new campground to the DB
+    Campground.create(newCampGround, function(err, newlyCreated){
+        if(err) {
+            console.log('Error trying to add a campground to DB! ' + err);
+        } else {
+            // Redirect to campgrounds page
+            res.redirect('/campgrounds'); // Default redirection is as GET request
+        }
+    });
 });
 app.get('/campgrounds/new', (req, res) => { // Using RESTful convention
     // res.send('Create a new camp!');
